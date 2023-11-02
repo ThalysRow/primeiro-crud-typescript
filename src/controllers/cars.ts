018c54fceb86bd8abfa8e809bcd02323b0bff717
Thalys Row <123907051+ThalysRow@users.noreply.github.com>
2023-11-01 23:11:10 -0300

    Crud de carros finalizado

--- src/controllers/cars.ts
+++ src/controllers/cars.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { knex } from "../database/conextion";
+import { Car } from "../types/tyes";
 
 export const listCars = async (_: Request, res: Response) => {
   try {
-    const result = await knex("cars");
+    const result = await knex<Car>("cars");
     return res.status(200).json(result);
   } catch (error) {
     return res.status(500).json({ message: "Error in list cars." });
@@ -13,7 +14,7 @@ export const listCars = async (_: Request, res: Response) => {
 export const detailCar = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const car = await knex("cars").where("id", id).first();
+    const car = await knex<Car>("cars").where("id", id).first();
     return res.status(200).json(car);
   } catch (error) {
     return res.status(500).json({ message: "Error in detail car." });
@@ -24,10 +25,10 @@ export const updateCar = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { brand, model, yearcar, collor, price } = req.body;
   try {
-    await knex("cars")
+    await knex<Car>("cars")
       .update({ brand, model, yearcar, collor, price })
       .where("id", id);
-    return res.status(201).json({ message: "update completed successfully." });
+    return res.status(204).send();
   } catch (error) {
     return res.status(500).json({ message: "Error in update car." });
   }
@@ -36,8 +37,8 @@ export const updateCar = async (req: Request, res: Response) => {
 export const deleteCar = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    await knex("cars").where("id", id).del();
-    return res.status(200).json({ message: "delete completed sucessfully." });
+    await knex<Car>("cars").where("id", id).del();
+    return res.status(204).send();
   } catch (error) {
     return res.status(500).json({ message: "Error in delete car." });
   }
@@ -46,7 +47,13 @@ export const deleteCar = async (req: Request, res: Response) => {
 export const createCar = async (req: Request, res: Response) => {
   const { brand, model, yearcar, collor, price } = req.body;
   try {
-    await knex("cars").insert({ brand, model, yearcar, collor, price });
+    await knex<Omit<Car, "id">>("cars").insert({
+      brand,
+      model,
+      yearcar,
+      collor,
+      price,
+    });
     return res.status(201).json({ message: "create sucessfully." });
   } catch (error) {
     return res.status(500).json({ message: "Error in create car." });
---
+++ src/types/tyes.ts
@@ -0,0 +1,8 @@
+export type Car = {
+  id: number;
+  brand: string;
+  model: string;
+  yearcar: number;
+  collor: string;
+  price: number;
+};
