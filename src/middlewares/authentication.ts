a83cb2122f0262f5ac6951597e68cee15f83c3de
Thalys Row <123907051+ThalysRow@users.noreply.github.com>
2023-11-04 18:18:02 -0300

    Implementado autenticação ao logar

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { knex } from "../database/conextion";
+import { User } from "../types/tyes";
+interface CustomRequest extends Request {
+  userId?: number; // Adicione a propriedade 'userId' ao Request
+}
+
+export const authentication = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+  try {
+    if (!authorization) {
+      return res.status(401).json({ message: "unauthenticated" });
+    } else {
+      const token = authorization.split(" ")[1];
+      const user = jwt.verify(token, process.env.JWT_PASS) as JwtPayload;
+      req.userId = user.id;
+
+      const userFind = await knex<User>("users")
+        .where("id", req.userId)
+        .first();
+      if (!userFind) {
+        return res.status(401).json({ message: "unauthenticated" });
+      }
+      next();
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error in authentication" });
+  }
+};
--- src/routes.ts
+++ src/routes.ts
@@ -7,11 +7,14 @@ import {
   updateCar,
 } from "./controllers/cars";
 import { createUser, loginUser } from "./controllers/users";
+import { authentication } from "./middlewares/authentication";
 
 const routes = Router();
 
 routes.post("/user", createUser);
 routes.post("/login", loginUser);
+
+routes.use(authentication);
 
 routes.get("/cars", listCars);
 routes.get("/car/:id", detailCar);
